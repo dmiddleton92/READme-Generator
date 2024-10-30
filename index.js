ca35bc512929387ff1fc28ec87b1b1d4ac3c0aca
dmiddleton92 <dm.middleton92@gmail.com>
2024-10-30 16:40:16 -0600

    moved inquirer into function init

--- index.js
+++ index.js
@@ -2,49 +2,7 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
 import generateMarkdown from './utils/generateMarkdown.js';
-import { file } from 'tmp';
 
-// TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please write a short description of your project:',
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: "Please provide installation instructions for your project:",
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Please provide instructions and examples for use:',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What kind of license should your project have?',
-            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-        },
-        {
-            type: 'input',
-            name: 'Repo',
-            message: 'What is your GitHub repo name?',
-        }
-
-    ])
-    .then((data) => {
-        const filename = `${data.project.toLowerCase().split(' ').join('')}.md`;
-writeToFile(filename, data);
-    });
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, generateMarkdown(data), (err) =>
@@ -53,7 +11,48 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your project?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Please write a short description of your project:',
+            },
+            {
+                type: 'input',
+                name: 'Installation',
+                message: "Please provide installation instructions for your project:",
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Please provide instructions and examples for use:',
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'What kind of license should your project have?',
+                choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+            },
+            {
+                type: 'input',
+                name: 'Repo',
+                message: 'What is your GitHub repo name?',
+            }
+            //need to add project with in prompt**
+
+        ])
+        .then((data) => {
+            const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+            writeToFile(filename, data);
+        });
+}
 
 // Function call to initialize app
 init();
